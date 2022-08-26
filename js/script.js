339f34e368e019a231d60a195284be200ab8ae78
Jeko26 <zennanovozilov@yandex.ru>
2022-08-27 01:12:19 +0300

    raspechator.github.io

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var swiper = new Swiper(".swiper__container", {
+    cssMode: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: 'bullets',
+      clickable: true
+    },
+    // autoplay: {
+    //     delay: 4000,
+    //     disableOnInteraction: false
+    // },
+    mousewheel: true,
+    keyboard: true,
+  });
+
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [43.127183, 131.940255],
+            zoom: 11,
+            controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark([43.127183, 131.940255], {
+            hintContent: 'Рыбинск?',
+            balloonContent: 'Рыбинск!'
+        }, {
+            iconLayout: 'default#image',
+            iconImageSize: [40, 40],
+            iconImageOffset: [-5, -38]
+        })
+
+    myMap.geoObjects
+        .add(myPlacemark)
+});
